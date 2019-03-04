9aacc0b57eab56eae74f4b46dfda800cd1f4124a
gilsonalves <gilsonalves.developer@gmail.com>
2019-03-04 01:11:16 -0300

    Alterando layout

--- app/src/main/java/com/example/vitorpereira/projetofinal_android_ufg/MainActivity.java
+++ app/src/main/java/com/example/vitorpereira/projetofinal_android_ufg/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.vitorpereira.projetofinal_android_ufg;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -31,7 +37,7 @@ import com.google.firebase.storage.StorageReference;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private FirebaseAuth firebaseAuth;
     DatabaseReference dataBaseReference;
@@ -45,28 +51,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cadastrarReceita(this);
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
         inicializeFirebase();
 
         listaDados();
 
         firebaseAuth = FirebaseAuth.getInstance();
+    }
 
-        Button deleteAccount = findViewById(R.id.delete_account_button);
-
-        deleteAccount.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AuthUI.getInstance().delete(MainActivity.this).addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if (task.isSuccessful()){
-                            Toast.makeText(MainActivity.this,"Your account has been deleted. ",Toast.LENGTH_SHORT).show();
-                            signOut();
-                        }
-                    }
-                });
-            }
-        });
+    private void cadastrarReceita(View.OnClickListener onClickListener) {
 
     }
 
@@ -83,7 +97,7 @@ public class MainActivity extends AppCompatActivity {
                 ListView lista = (ListView) findViewById(R.id.lvMain);
 
                 ArrayAdapterReceita adapter = new ArrayAdapterReceita(MainActivity.this, listaReceitas);
-                lista.setAdapter(adapter);;
+                lista.setAdapter(adapter);
             }
 
             @Override
@@ -97,6 +111,32 @@ public class MainActivity extends AppCompatActivity {
         FirebaseApp.initializeApp(this);
         dataBaseReference = FirebaseDatabase.getInstance().getReference();
         storageReference = FirebaseStorage.getInstance().getReference();
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_receitas) {
+            //carregaListaPlanetas();
+        } else if (id == R.id.nav_favoritos) {
+            //carregaListaPersonagens();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @Override
